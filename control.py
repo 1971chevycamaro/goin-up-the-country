933589a1388e32fb8ee085569f322f3f9984a8d4
1971chevycamaro <skylord.goopes45@gmail.com>
2017-11-14 14:22:18 -0600

    Update control.py
    cleaned up and reduced CPU usage with pygame.time.wait(1)

--- control.py
+++ control.py
@@ -1,37 +1,27 @@
-print("importing modules...")
 import pigpio
-import pygame
-from time import sleep
-print("initiating window...")
-pygame.init()
-maintimer = pygame.clock.Clock()
-maintimer.tick(24)
-screen = pygame.display.set_mode((100,100))
-quit = False
-pi = pigpio.pi()
-if not pi.connected:
-    exit()
-else:
-    print("connected")
-print("executing script...")
-while not quit:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                pi.set_servo_pulsewidth(17,1800)
-            elif event.key == pygame.K_d:
-                pi.set_servo_pulsewidth(17,1200)
-            elif event.key == pygame.K_w:
-                pi.set_servo_pulsewidth(18,1600)
-            elif event.key == pygame.K_s:
-                pi.set_servo_pulsewidth(18,1400)
-            elif event.key == pygame.K_ESCAPE:
-                pi.set_servo_pulsewidth(17,0)
-                quit = True        
-        if event.type == pygame.KEYUP:
-            if event.key in [pygame.K_a, pygame.K_d]:
-                pi.set_servo_pulsewidth(17,1500)
-            if event.key in [pygame.K_w, pygame.K_s]:
-                pi.set_servo_pulsewidth(18,1500)
-
-pi.stop()
+from pygame import *
+display.init()
+scr = display.set_mode((720,480))
+run = True
+while run:
+    for e in event.get():
+        if e.type == KEYDOWN:
+            if e.key == K_a:
+                var = 17, 1800
+            elif e.key == K_d:
+                var = 17, 1200
+            elif e.key == K_w:
+                var = 18, 1600
+            elif e.key == K_s:
+                var = 18, 1400
+            elif e.key == K_ESCAPE:
+                var = 17, 0
+                run = False        
+        if e.type == KEYUP:
+            if e.key in [K_a, K_d]:
+                var = 17, 1500
+            if e.key in [K_w, K_s]:
+                var = 18, 1500
+        pigpio.pi.set_servo_pulsewidth(var)
+    time.wait(1)
+pigpio.stop()
