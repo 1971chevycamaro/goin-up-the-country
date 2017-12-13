f34c708eed543b8e867d3d0bb4fc5b7127258308
1971chevycamaro <skylord.goopes45@gmail.com>
2017-12-12 22:06:14 -0600

    Updated esc.py and created new branch

--- car/esc.py
+++ car/esc.py
@@ -1,17 +1,28 @@
-import pigpio
-reportErrors = True
-def setThrottle(speed):
-  pin = 18
+import time, threading, pigpio#, ease
+pi = pigpio.pi()
+pin = 18
+def setThrottle(speed = 1500, arg1 = None, arg2 = None, reportErrors = False):
   presets = ["forward","backward","neutral"]
+  if arg1 == "base":
+    #print(pin, speed + 1500)
+    pi.set_servo_pulsewidth(pin, speed + 1500)
   if isinstance(speed, str) and speed in presets:
     pwm = [1600,1400,1500]
-    pigpio.set_servo_pulsewidth(pin,pwm[presets.index(speed)])
+    #print(pin, pwm[presets.index(speed)])
+    pi.set_servo_pulsewidth(pin, pwm[presets.index(speed)])
   elif isinstance(speed, int) and speed in range(1000,2001):
     pwm = speed
-    pigpio.set_servo_pulsewidth(pin,pwm)
+    #print(pin, pwm)
+    pi.set_servo_pulsewidth(pin, pwm)
   #Error Handler
   else:
     if reportErrors:
       print("speed is invalid or incorrect type %s" % type(speed))
-def getThrottle():
-  return pigpio.get_servo_pulsewidth(18)
+def getThrottle(arg1 = None):
+  if arg1 == "base":
+    #print("pi.get_servo_pulsewidth(%s) - 1500" % pin)
+    return pi.get_servo_pulsewidth(pin)
+  elif arg1 == None:
+    #print("pi.get_servo_pulsewidth(pin)")
+    return pi.get_servo_pulsewidth(pin)
+  
